e1eebe63f44d77495aeff2d6be029777438ec6a2
Abdul-Manan-PG <manan12345ch@gmail.com>
2025-07-22 22:47:20 +0500

    Update Recursion_Basics.cpp

--- Recursion_Basics.cpp
+++ Recursion_Basics.cpp
@@ -7,11 +7,27 @@ using namespace std;
 
 // 1. fibonacci function
 
-int fibonacci(int n) {
-    if (n <= 1) return n;
-    cout << "Calculating fibonacci(" << n << ")" << endl;
-    // Recursive call to calculate fibonacci of n-1 and n-2
-    return fibonacci(n - 1) + fibonacci(n - 2);
+// int fibonacci(int n) {
+//     if (n <= 1) return n;
+//     cout << "Calculating fibonacci(" << n << ")" << endl;
+//     // Recursive call to calculate fibonacci of n-1 and n-2
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+// }
+
+// more better function space increase but stack overflow chances are decreased
+
+int fibonacci(int n, int b = 1) {
+    static int a = 1;
+    if (n <= 1){
+        int temp = a;
+        a = 0;
+        return temp;
+    }
+    cout << a << ' ' << b << endl;
+    int temp = b;
+    b += a;
+    a = temp;
+    return fibonacci(n - 1, b);
 }
 
 int main() {
