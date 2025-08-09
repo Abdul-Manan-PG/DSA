f549ec86979998adb26ce2e76a227d8078a2734b
Abdul-Manan-PG <manan12345ch@gmail.com>
2025-08-09 15:56:44 +0500

    Matrixs

---
+++ 5. Matrixs/LeetCode_2022.cpp
@@ -0,0 +1,23 @@
+// 2022. Convert 1D array to 2D array 
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if (m * n != original.size()) {
+            return {};
+        }
+        vector<vector<int>> res(m, vector<int>(n));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i][j] = original[i * n + j];
+            }
+        }
+        return res;
+    }
+};
