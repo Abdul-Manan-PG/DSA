5a3159e8fdd42c5d4979325424b07f0c3ad53fcc
Abdul-Manan-PG <manan12345ch@gmail.com>
2025-08-11 23:18:14 +0500

    leetcode

---
+++ 5. Matrixs/LeetCode_48.cpp
@@ -0,0 +1,24 @@
+// 48. Rotate image
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for (int i = 0; i < m; i++){
+            for (int j = i + 1; j < n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n / 2; j++){
+                swap(matrix[i][j], matrix[i][n-1-j]);
+            }
+        }
+    }
+};
