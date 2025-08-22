9bcb94658af752272bdf8c993ff07dd91ac9b9d4
Abdul-Manan-PG <manan12345ch@gmail.com>
2025-08-22 15:38:58 +0500

    Binary search

---
+++ 11. Binary Search/LeetCode_704.cpp
@@ -0,0 +1,34 @@
+// 704. Binary Search
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int low = 0;
+        int high = arr.size() - 1;
+
+        while (low <= high) {
+            // Prevents potential integer overflow compared to (low + high) / 2
+            int mid = low + (high - low) / 2;
+
+            // Check if target is present at mid
+            if (arr[mid] == target) {
+                return mid; // Target found
+            }
+            // If target is greater, ignore left half
+            else if (arr[mid] < target) {
+                low = mid + 1;
+            }
+            // If target is smaller, ignore right half
+            else {
+                high = mid - 1;
+            }
+        }
+        // Target was not present in the array
+        return -1;
+    }
+};
