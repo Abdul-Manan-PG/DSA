cf7ce1df5c7673eb3511d8311789672c0af26f87
Abdul-Manan-PG <manan12345ch@gmail.com>
2025-08-26 14:55:11 +0500

    LeetCode problems

--- 12. Bit Manipulation/LeetCode_476.cpp
+++ 12. Bit Manipulation/LeetCode_476.cpp
@@ -1,2 +1,22 @@
 // 476. Number Compliment
 
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class Solution {
+public:
+    int findComplement(int num) {
+        if (num == 0) return 1;
+        int n = 0;
+        int temp = num;
+        while (temp) {
+            n++;
+            temp >>= 1;
+        }
+        int mask = (1LL << n) - 1;
+        return num ^ mask;
+    }
+};
