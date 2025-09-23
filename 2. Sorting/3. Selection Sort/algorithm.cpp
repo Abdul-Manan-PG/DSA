2b7688b44f9b11f2cbabf110ea12eb48413186b7
Abdul-Manan-PG <manan12345ch@gmail.com>
2025-09-23 13:57:54 +0500

    Modified

--- 2. Sorting/3. Selection Sort/algorithm.cpp
+++ 2. Sorting/3. Selection Sort/algorithm.cpp
@@ -3,43 +3,49 @@
 #include <string>
 using namespace std;
 
-void selectionSort(vector<int> &arr, bool desc = false) 
+void selectionSort(vector<int> &arr, bool desc = false)
 {
     for (int i = 0; i < arr.size() - 1; i++)
     {
         int target = i; // Index of min/max element
-        for (int j = i + 1; j < arr.size(); j++) 
+        for (int j = i + 1; j < arr.size(); j++)
         {
-            if (desc && arr[j] > arr[target]) 
+            if (dec)
             {
-                target = j;
-            } 
-            else if (!desc && arr[j] < arr[target]) 
+                if (arr[j] > arr[target])
+                    target = j;
+            }
+            else
             {
-                target = j;
+                if (arr[j] < arr[target])
+                    target = j;
             }
         }
         swap(arr[i], arr[target]);
     }
 }
 
-int main() {
+int main()
+{
     vector<int> arr = {64, 25, 12, 22, 11};
-    
+
     cout << "Original array: ";
-    for (int num : arr) cout << num << " ";
+    for (int num : arr)
+        cout << num << " ";
     cout << endl;
 
     // Ascending order (default)
     selectionSort(arr);
     cout << "Ascending order: ";
-    for (int num : arr) cout << num << " ";
+    for (int num : arr)
+        cout << num << " ";
     cout << endl;
 
     // Descending order
     selectionSort(arr, true);
     cout << "Descending order: ";
-    for (int num : arr) cout << num << " ";
+    for (int num : arr)
+        cout << num << " ";
     cout << endl;
 
     return 0;
