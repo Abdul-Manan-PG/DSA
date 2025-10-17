e6dc279874459586f72a71fce6d8f0f052050f64
Abdul-Manan-PG <manan12345ch@gmail.com>
2025-10-17 19:40:49 +0500

    BST

---
+++ 15. Binary Search Tree/3. Validation and Properties/Leetcode_783.cpp
@@ -0,0 +1,35 @@
+// 783. Minimum distance between BST Nodes
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void solve(TreeNode* root, int& ans, TreeNode*& prev) {
+        if (!root)
+            return;
+        solve(root->left, ans, prev);
+        if (prev)
+            ans = min(ans, root->val - prev->val);
+        prev = root;
+        solve(root->right, ans, prev);
+    }
+    int minDiffInBST(TreeNode* root) {
+        TreeNode* prev = nullptr;
+        int ans = INT_MAX;
+        solve(root, ans, prev);
+        return ans;
+    }
+};
