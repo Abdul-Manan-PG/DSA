5d39520a14fd13ec63e983103aada7db32c7ca17
Abdul-Manan-PG <manan12345ch@gmail.com>
2025-10-31 20:30:14 +0500

    daily

---
+++ 1. DSA fundamentals/Daily problems Leetcode/Leetcode_2089.cpp
@@ -0,0 +1,18 @@
+// 2089. Find Target Indices After Sorting Array
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<int> ans;
+        for (int i = 0; i < nums.size();i++){
+            if (nums[i] == target) ans.push_back(i);
+        }
+        return ans;
+    }
+};
