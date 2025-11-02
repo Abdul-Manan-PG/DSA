5fc12d8ffefb673b46f5de4df23a7675037e18d8
Abdul-Manan-PG <manan12345ch@gmail.com>
2025-11-02 09:34:09 +0500

    problems

---
+++ 15. Binary Search Tree/3. Validation and Properties/Leetcode_96.cpp
@@ -0,0 +1,24 @@
+// 96. Unique Binary Search Tree
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> uniqTree(n + 1, 1);
+
+        for (int nodes = 2; nodes <= n; nodes++) {
+            int total = 0;
+            for (int root = 1; root <= nodes; root++) {
+                total += uniqTree[root - 1] * uniqTree[nodes - root];
+            }
+            uniqTree[nodes] = total;
+        }
+
+        return uniqTree[n];        
+    }
+};
+
